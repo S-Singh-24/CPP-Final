19fc18e6ea4f59c5f4eea07c32ac950124183872
S-Singh-24 <94930298+S-Singh-24@users.noreply.github.com>
2025-04-11 18:27:15 -0400

    Added some necessary files, and mostly completed product.h

---
+++ product.h
@@ -0,0 +1,32 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include <string>
+
+class Product {
+private:
+    int productID;
+    std::string name;
+    double price;
+    int quantityInStock;
+    
+public:
+    Product(int id, std::string name, double price, int quantity);
+    
+    //getter
+    int getProductID() const;
+    std::string getName() const;
+    double getPrice() const;
+    int getQuantity() const;
+    
+    //setters
+    void setName(std::string newName);
+    void setPrice(double newPrice);
+    void setQuantity(int newQuantity);
+    
+    //methods
+    void decreaseQuantity(int amount);
+    void increaseQuantity(int amount);
+};
+
+#endif // PRODUCT_H
